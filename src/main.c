05871785d886ce70cad5c8a54947b0f76efe0e2b
Adam Patterson <adam@adamrt.com>
2024-09-27 13:27:04 -0700

    Split functions into separate components and split cube.h

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include <math.h>
 
+#include "cglm/struct.h"
+
 #include "sokol_app.h"
 #include "sokol_gfx.h"
 #include "sokol_glue.h"
 #include "sokol_log.h"
 
-#include "cglm/struct.h"
-
 #include "shader.glsl.h"
+
+#include "cube.h"
 
 // application state
 static struct {
@@ -17,24 +19,34 @@ static struct {
         sg_pass_action pass_action;
     } gfx;
 
-    float rx, ry;
+    struct {
+        mat4s mvp;
+        float rx, ry;
+    } model;
+
 } state;
 
 // forward declarations
-static void init(void);
-static void event(const sapp_event* e);
-static void frame(void);
-static void cleanup(void);
+static void engine_init(void);
+static void engine_event(const sapp_event* event);
+static void engine_update(void);
+static void engine_cleanup(void);
+
+static void gfx_init(void);
+static void gfx_frame_begin(void);
+static void gfx_frame_end(void);
+
+static void update(void);
 
 sapp_desc sokol_main(int argc, char* argv[])
 {
     (void)argc;
     (void)argv;
     return (sapp_desc) {
-        .init_cb = init,
-        .event_cb = event,
-        .frame_cb = frame,
-        .cleanup_cb = cleanup,
+        .init_cb = engine_init,
+        .event_cb = engine_event,
+        .frame_cb = engine_update,
+        .cleanup_cb = engine_cleanup,
         .width = 1280,
         .height = 1024,
         .window_title = "Starterkit",
@@ -43,70 +55,78 @@ sapp_desc sokol_main(int argc, char* argv[])
     };
 }
 
-static void init(void)
+static void engine_init(void)
+{
+    gfx_init();
+}
+
+static void engine_event(const sapp_event* event)
+{
+    if (event->type == SAPP_EVENTTYPE_KEY_DOWN) {
+        if (event->key_code == SAPP_KEYCODE_ESCAPE) {
+            sapp_request_quit();
+        }
+    }
+}
+
+static void engine_update(void)
+{
+    update();
+
+    vs_params_t vs_params;
+    vs_params.mvp = state.model.mvp;
+
+    gfx_frame_begin();
+    {
+        sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_params, &SG_RANGE(vs_params));
+        sg_draw(0, 36, 1);
+    }
+    gfx_frame_end();
+}
+
+static void engine_cleanup(void)
+{
+    sg_shutdown();
+}
+
+static void update(void)
+{
+    const float w = sapp_widthf();
+    const float h = sapp_heightf();
+    const float t = (float)sapp_frame_duration();
+    state.model.rx += 1.0f * t;
+    state.model.ry += 2.0f * t;
+    mat4s rxm = glms_rotate_make(state.model.rx, GLMS_XUP);
+    mat4s rym = glms_rotate_make(state.model.ry, GLMS_YUP);
+
+    mat4s proj = glms_perspective(glm_rad(60.0f), w / h, 0.01f, 100.0f);
+
+    vec3s eye = { { 0.0f, 1.5f, 6.0f } };
+    vec3s center = { { 0.0f, 0.0f, 0.0f } };
+    vec3s up = { { 0.0f, 1.0f, 0.0f } };
+    mat4s view = glms_lookat(eye, center, up);
+
+    mat4s view_proj = glms_mat4_mul(proj, view);
+    mat4s model = glms_mat4_mul(rxm, rym);
+
+    state.model.mvp = glms_mat4_mul(view_proj, model);
+}
+
+static void gfx_init(void)
 {
     sg_setup(&(sg_desc) {
         .environment = sglue_environment(),
         .logger.func = slog_func,
     });
 
-    /* cube vertex buffer */
-    // clang-format off
-    float vertices[] = {
-        -1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
-         1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
-         1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
-        -1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
-
-        -1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
-         1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
-         1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
-        -1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
-
-        -1.0, -1.0, -1.0,   0.0, 0.0, 1.0, 1.0,
-        -1.0,  1.0, -1.0,   0.0, 0.0, 1.0, 1.0,
-        -1.0,  1.0,  1.0,   0.0, 0.0, 1.0, 1.0,
-        -1.0, -1.0,  1.0,   0.0, 0.0, 1.0, 1.0,
-
-         1.0, -1.0, -1.0,   1.0, 0.5, 0.0, 1.0,
-         1.0,  1.0, -1.0,   1.0, 0.5, 0.0, 1.0,
-         1.0,  1.0,  1.0,   1.0, 0.5, 0.0, 1.0,
-         1.0, -1.0,  1.0,   1.0, 0.5, 0.0, 1.0,
-
-        -1.0, -1.0, -1.0,   0.0, 0.5, 1.0, 1.0,
-        -1.0, -1.0,  1.0,   0.0, 0.5, 1.0, 1.0,
-         1.0, -1.0,  1.0,   0.0, 0.5, 1.0, 1.0,
-         1.0, -1.0, -1.0,   0.0, 0.5, 1.0, 1.0,
-
-        -1.0,  1.0, -1.0,   1.0, 0.0, 0.5, 1.0,
-        -1.0,  1.0,  1.0,   1.0, 0.0, 0.5, 1.0,
-         1.0,  1.0,  1.0,   1.0, 0.0, 0.5, 1.0,
-         1.0,  1.0, -1.0,   1.0, 0.0, 0.5, 1.0
-    };
-    // clang-format on
-
     sg_buffer vbuf = sg_make_buffer(&(sg_buffer_desc) {
         .data = SG_RANGE(vertices),
         .label = "cube-vertices" });
-
-    /* create an index buffer for the cube */
-    // clang-format off
-    uint16_t indices[] = {
-        0, 1, 2,  0, 2, 3,
-        6, 5, 4,  7, 6, 4,
-        8, 9, 10,  8, 10, 11,
-        14, 13, 12,  15, 14, 12,
-        16, 17, 18,  16, 18, 19,
-        22, 21, 20,  23, 22, 20
-    };
-    // clang-format on
 
     sg_buffer ibuf = sg_make_buffer(&(sg_buffer_desc) {
         .type = SG_BUFFERTYPE_INDEXBUFFER,
         .data = SG_RANGE(indices),
         .label = "cube-indices" });
-
-    sg_shader shd = sg_make_shader(cube_shader_desc(sg_query_backend()));
 
     state.gfx.pip = sg_make_pipeline(&(sg_pipeline_desc) {
         .layout = {
@@ -116,7 +136,7 @@ static void init(void)
                 [ATTR_vs_color0].format = SG_VERTEXFORMAT_FLOAT4,
             },
         },
-        .shader = shd,
+        .shader = sg_make_shader(cube_shader_desc(sg_query_backend())),
         .index_type = SG_INDEXTYPE_UINT16,
         .cull_mode = SG_CULLMODE_NONE,
         .depth = {
@@ -132,38 +152,8 @@ static void init(void)
     };
 }
 
-static void event(const sapp_event* e)
+static void gfx_frame_begin(void)
 {
-    if (e->type == SAPP_EVENTTYPE_KEY_DOWN) {
-        if (e->key_code == SAPP_KEYCODE_ESCAPE) {
-            sapp_request_quit();
-        }
-    }
-}
-
-static void frame(void)
-{
-    vs_params_t vs_params;
-    const float w = sapp_widthf();
-    const float h = sapp_heightf();
-    const float t = (float)sapp_frame_duration();
-    state.rx += 1.0f * t;
-    state.ry += 2.0f * t;
-    mat4s rxm = glms_rotate_make(state.rx, GLMS_XUP);
-    mat4s rym = glms_rotate_make(state.ry, GLMS_YUP);
-
-    mat4s proj = glms_perspective(glm_rad(60.0f), w / h, 0.01f, 100.0f);
-
-    vec3s eye = { { 0.0f, 1.5f, 6.0f } };
-    vec3s center = { { 0.0f, 0.0f, 0.0f } };
-    vec3s up = { { 0.0f, 1.0f, 0.0f } };
-    mat4s view = glms_lookat(eye, center, up);
-
-    mat4s view_proj = glms_mat4_mul(proj, view);
-    mat4s model = glms_mat4_mul(rxm, rym);
-
-    vs_params.mvp = glms_mat4_mul(view_proj, model);
-
     sg_begin_pass(&(sg_pass) {
         .action = {
             .colors[0] = {
@@ -176,13 +166,10 @@ static void frame(void)
 
     sg_apply_pipeline(state.gfx.pip);
     sg_apply_bindings(&state.gfx.bind);
-    sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_params, &SG_RANGE(vs_params));
-    sg_draw(0, 36, 1);
-    sg_end_pass();
-    sg_commit();
 }
 
-static void cleanup(void)
+static void gfx_frame_end(void)
 {
-    sg_shutdown();
+    sg_end_pass();
+    sg_commit();
 }
