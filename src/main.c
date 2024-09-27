0697653a8303014383069297802494df0742203c
Adam Patterson <adam@adamrt.com>
2024-09-26 17:55:12 -0700

    Add cglm and show a rotating cube

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
+#include <math.h>
+
 #include "sokol_app.h"
 #include "sokol_gfx.h"
 #include "sokol_glue.h"
 #include "sokol_log.h"
+
+#include "cglm/struct.h"
 
 #include "shader.glsl.h"
 
@@ -12,6 +16,8 @@ static struct {
         sg_bindings bind;
         sg_pass_action pass_action;
     } gfx;
+
+    float rx, ry;
 } state;
 
 // forward declarations
@@ -42,43 +48,125 @@ static void init(void)
         .logger.func = slog_func,
     });
 
+    /* cube vertex buffer */
+    // clang-format off
     float vertices[] = {
-        // positions            // colors
-        0.0f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f,
-        0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f,
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f
-    };
-    state.gfx.bind.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc) {
-        .data = SG_RANGE(vertices),
-        .label = "triangle-vertices" });
+        -1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+         1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+         1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+        -1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
 
-    sg_shader shd = sg_make_shader(triangle_shader_desc(sg_query_backend()));
+        -1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+         1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+         1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+        -1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+
+        -1.0, -1.0, -1.0,   0.0, 0.0, 1.0, 1.0,
+        -1.0,  1.0, -1.0,   0.0, 0.0, 1.0, 1.0,
+        -1.0,  1.0,  1.0,   0.0, 0.0, 1.0, 1.0,
+        -1.0, -1.0,  1.0,   0.0, 0.0, 1.0, 1.0,
+
+         1.0, -1.0, -1.0,   1.0, 0.5, 0.0, 1.0,
+         1.0,  1.0, -1.0,   1.0, 0.5, 0.0, 1.0,
+         1.0,  1.0,  1.0,   1.0, 0.5, 0.0, 1.0,
+         1.0, -1.0,  1.0,   1.0, 0.5, 0.0, 1.0,
+
+        -1.0, -1.0, -1.0,   0.0, 0.5, 1.0, 1.0,
+        -1.0, -1.0,  1.0,   0.0, 0.5, 1.0, 1.0,
+         1.0, -1.0,  1.0,   0.0, 0.5, 1.0, 1.0,
+         1.0, -1.0, -1.0,   0.0, 0.5, 1.0, 1.0,
+
+        -1.0,  1.0, -1.0,   1.0, 0.0, 0.5, 1.0,
+        -1.0,  1.0,  1.0,   1.0, 0.0, 0.5, 1.0,
+         1.0,  1.0,  1.0,   1.0, 0.0, 0.5, 1.0,
+         1.0,  1.0, -1.0,   1.0, 0.0, 0.5, 1.0
+    };
+    // clang-format on
+
+    sg_buffer vbuf = sg_make_buffer(&(sg_buffer_desc) {
+        .data = SG_RANGE(vertices),
+        .label = "cube-vertices" });
+
+    /* create an index buffer for the cube */
+    // clang-format off
+    uint16_t indices[] = {
+        0, 1, 2,  0, 2, 3,
+        6, 5, 4,  7, 6, 4,
+        8, 9, 10,  8, 10, 11,
+        14, 13, 12,  15, 14, 12,
+        16, 17, 18,  16, 18, 19,
+        22, 21, 20,  23, 22, 20
+    };
+    // clang-format on
+
+    sg_buffer ibuf = sg_make_buffer(&(sg_buffer_desc) {
+        .type = SG_BUFFERTYPE_INDEXBUFFER,
+        .data = SG_RANGE(indices),
+        .label = "cube-indices" });
+
+    sg_shader shd = sg_make_shader(cube_shader_desc(sg_query_backend()));
 
     state.gfx.pip = sg_make_pipeline(&(sg_pipeline_desc) {
-        .shader = shd,
         .layout = {
+            .buffers[0].stride = 28,
             .attrs = {
                 [ATTR_vs_position].format = SG_VERTEXFORMAT_FLOAT3,
                 [ATTR_vs_color0].format = SG_VERTEXFORMAT_FLOAT4,
             },
         },
-        .label = "triangle-pipeline",
+        .shader = shd,
+        .index_type = SG_INDEXTYPE_UINT16,
+        .cull_mode = SG_CULLMODE_NONE,
+        .depth = {
+            .write_enabled = true,
+            .compare = SG_COMPAREFUNC_LESS_EQUAL,
+        },
+        .label = "cube-pipeline",
     });
 
-    state.gfx.pass_action = (sg_pass_action) {
-        .colors[0] = {
-            .load_action = SG_LOADACTION_CLEAR,
-            .clear_value = { 0.0f, 0.0f, 0.0f, 1.0f },
-        }
+    state.gfx.bind = (sg_bindings) {
+        .vertex_buffers[0] = vbuf,
+        .index_buffer = ibuf
     };
 }
 
 static void frame(void)
 {
-    sg_begin_pass(&(sg_pass) { .action = state.gfx.pass_action, .swapchain = sglue_swapchain() });
+    vs_params_t vs_params;
+    const float w = sapp_widthf();
+    const float h = sapp_heightf();
+    const float t = (float)sapp_frame_duration();
+    state.rx += 1.0f * t;
+    state.ry += 2.0f * t;
+    mat4s rxm = glms_rotate_make(state.rx, GLMS_XUP);
+    mat4s rym = glms_rotate_make(state.ry, GLMS_YUP);
+
+    mat4s proj = glms_perspective(glm_rad(60.0f), w / h, 0.01f, 100.0f);
+
+    vec3s eye = { { 0.0f, 1.5f, 6.0f } };
+    vec3s center = { { 0.0f, 0.0f, 0.0f } };
+    vec3s up = { { 0.0f, 1.0f, 0.0f } };
+    mat4s view = glms_lookat(eye, center, up);
+
+    mat4s view_proj = glms_mat4_mul(proj, view);
+    mat4s model = glms_mat4_mul(rxm, rym);
+
+    vs_params.mvp = glms_mat4_mul(view_proj, model);
+
+    sg_begin_pass(&(sg_pass) {
+        .action = {
+            .colors[0] = {
+                .load_action = SG_LOADACTION_CLEAR,
+                .clear_value = { 0.25f, 0.5f, 0.75f, 1.0f },
+            },
+        },
+        .swapchain = sglue_swapchain(),
+    });
+
     sg_apply_pipeline(state.gfx.pip);
     sg_apply_bindings(&state.gfx.bind);
-    sg_draw(0, 3, 1);
+    sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_params, &SG_RANGE(vs_params));
+    sg_draw(0, 36, 1);
     sg_end_pass();
     sg_commit();
 }
