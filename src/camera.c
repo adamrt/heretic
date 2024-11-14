f07c3ccd9c43b636c5ff7e24093a8f732cd3c312
Adam Patterson <adam@adamrt.com>
2024-11-14 10:59:52 -0600

    Improve camera_update_proj calls

--- src/camera.c
+++ src/camera.c
@@ -2,13 +2,16 @@
 
 #include "camera.h"
 
+#define SENSITIVITY (0.02f)
+
+#define ZNEAR (-10.0f)
+#define ZFAR  (10.0f)
+
+#define MIN_DIST (0.5f)
+#define MAX_DIST (5.0f)
+
 #define MAX_ELEVATION (M_PI_2 - 0.01f)
 #define MIN_ELEVATION (-M_PI_2 - 0.01f)
-#define MIN_DIST      (0.5f)
-#define MAX_DIST      (5.0f)
-#define SENSITIVITY   (0.02f)
-#define ZNEAR         (-10.0f)
-#define ZFAR          (10.0f)
 
 typedef struct {
     mat4s proj;
@@ -25,6 +28,8 @@ typedef struct {
 
 static camera_t cam;
 
+static void camera_update_proj(void);
+
 void camera_init(void)
 {
     cam.center = (vec3s) { { 0.0f, 0.0f, 0.0f } };
@@ -34,6 +39,17 @@ void camera_init(void)
     cam.up = (vec3s) { { 0.0f, 1.0f, 0.0f } };
 
     camera_update();
+    camera_update_proj();
+}
+
+void camera_update(void)
+{
+    float x = cosf(cam.elevation) * sinf(cam.azimuth);
+    float y = sinf(cam.elevation);
+    float z = cosf(cam.elevation) * cosf(cam.azimuth);
+
+    cam.eye = (vec3s) { { cam.center.x + x, cam.center.y + y, cam.center.z + z } };
+    cam.view = glms_lookat(cam.eye, cam.center, cam.up);
 }
 
 static void camera_update_proj(void)
@@ -41,21 +57,7 @@ static void camera_update_proj(void)
     float aspect = sapp_widthf() / sapp_heightf();
     float w = cam.zoom_factor;
     float h = w / aspect;
-
     cam.proj = glms_ortho(-w, w, -h, h, ZNEAR, ZFAR);
-}
-
-void camera_update(void)
-{
-    // Convert azimuth and elevation (in radians) to spherical coordinates
-    float x = cosf(cam.elevation) * sinf(cam.azimuth);
-    float y = sinf(cam.elevation);
-    float z = cosf(cam.elevation) * cosf(cam.azimuth);
-
-    cam.eye = (vec3s) { { cam.center.x + x, cam.center.y + y, cam.center.z + z } };
-    cam.view = glms_lookat(cam.eye, cam.center, cam.up);
-
-    camera_update_proj();
 }
 
 void camera_rotate(float delta_azimuth, float delta_elevation)
@@ -69,6 +71,8 @@ void camera_zoom(float delta)
 {
     cam.zoom_factor -= delta * SENSITIVITY;
     cam.zoom_factor = glm_clamp(cam.zoom_factor, MIN_DIST, MAX_DIST);
+
+    camera_update_proj(); // Zoom affects the projection matrix
 }
 
 mat4s camera_get_proj(void) { return cam.proj; }
