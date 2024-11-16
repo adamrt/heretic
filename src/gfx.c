2fd0a2c97293794a97b8c2f33972d7abe6bb88a7
Adam Patterson <adam@adamrt.com>
2024-11-16 16:02:30 -0600

    FIX! The znear/zfar issue was with background quad
    It was drawing it at z: -5 so it was in front of the camera.
    
    Thanks to Josh Hannafor for helping me figure this one out.

--- src/gfx.c
+++ src/gfx.c
@@ -186,10 +186,10 @@ static void init_background(void)
     // Fullscreen quad vertices, moved back a bit
     // clang-format off
     vertex_t quad_vertices[] = {
-        { .position = {{ -1.0f, -1.0f, 0.5f }}, .uv = {{ 0.0f, 1.0f }} }, // bottom-left
-        { .position = {{  1.0f, -1.0f, 0.5f }}, .uv = {{ 1.0f, 1.0f }} }, // bottom-right
-        { .position = {{ -1.0f,  1.0f, 0.5f }}, .uv = {{ 0.0f, 0.0f }} }, // top-left
-        { .position = {{  1.0f,  1.0f, 0.5f }}, .uv = {{ 1.0f, 0.0f }} }  // top-right
+        { .position = {{ -1.0f, -1.0f, 1.0f }}, .uv = {{ 0.0f, 1.0f }} }, // bottom-left
+        { .position = {{  1.0f, -1.0f, 1.0f }}, .uv = {{ 1.0f, 1.0f }} }, // bottom-right
+        { .position = {{ -1.0f,  1.0f, 1.0f }}, .uv = {{ 0.0f, 0.0f }} }, // top-left
+        { .position = {{  1.0f,  1.0f, 1.0f }}, .uv = {{ 1.0f, 0.0f }} }  // top-right
     };
 
     uint16_t quad_indices[] = { 0, 1, 2, 1, 3, 2 };
