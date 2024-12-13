ba15ffa4925a886665a4d029f9dd1370a784d343
Adam Patterson <adam@adamrt.com>
2024-12-13 16:00:12 -0600

    Use a macro for generating all the read functions

--- src/bin.c
+++ src/bin.c
@@ -6,60 +6,30 @@
 
 #define FILE_SIZE_MAX (131072)
 
+// FN_READ is a macro that generates a read function for a specific type. It
+// reads the value, returns it and increments the offset.
+#define FN_READ(type)                                      \
+    type read_##type(buffer_t* f)                          \
+    {                                                      \
+        type value;                                        \
+        memcpy(&value, &f->data[f->offset], sizeof(type)); \
+        f->offset += sizeof(type);                         \
+        return value;                                      \
+    }
+
+FN_READ(u8)
+FN_READ(u16)
+FN_READ(u32)
+FN_READ(i8)
+FN_READ(i16)
+FN_READ(i32)
+
 void read_bytes(buffer_t* f, usize size, u8* out_bytes)
 {
     ASSERT(size < FILE_SIZE_MAX, "File size too large");
     memcpy(out_bytes, &f->data[f->offset], size);
     f->offset += size;
     return;
-}
-
-u8 read_u8(buffer_t* f)
-{
-    u8 value;
-    memcpy(&value, &f->data[f->offset], sizeof(u8));
-    f->offset += sizeof(u8);
-    return value;
-}
-
-u16 read_u16(buffer_t* f)
-{
-    u16 value;
-    memcpy(&value, &f->data[f->offset], sizeof(u16));
-    f->offset += sizeof(u16);
-    return value;
-}
-
-u32 read_u32(buffer_t* f)
-{
-    u32 value;
-    memcpy(&value, &f->data[f->offset], sizeof(u32));
-    f->offset += sizeof(u32);
-    return value;
-}
-
-i8 read_i8(buffer_t* f)
-{
-    i8 value;
-    memcpy(&value, &f->data[f->offset], sizeof(i8));
-    f->offset += sizeof(i8);
-    return value;
-}
-
-i16 read_i16(buffer_t* f)
-{
-    i16 value;
-    memcpy(&value, &f->data[f->offset], sizeof(i16));
-    f->offset += sizeof(i16);
-    return value;
-}
-
-i32 read_i32(buffer_t* f)
-{
-    i32 value;
-    memcpy(&value, &f->data[f->offset], sizeof(i32));
-    f->offset += sizeof(i32);
-    return value;
 }
 
 f32 read_f1x3x12(buffer_t* f)
