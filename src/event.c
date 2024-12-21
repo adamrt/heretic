7efedd63188cc977a8aa55abd81c67cf2fb716f5
Adam Patterson <adam@adamrt.com>
2024-12-21 16:19:09 -0600

    Tidy up read_event for style preferences

--- src/event.c
+++ src/event.c
@@ -36,31 +36,26 @@ event_desc_t event_get_desc_by_scenario_id(int scenario_id) {
 }
 
 static event_t read_event(span_t* span) {
-    event_t event = { 0 };
+    event_t event = { .valid = false };
 
     u32 text_offset = span_read_u32(span);
-    bool valid = text_offset != 0xF2F2F2F2;
-    if (!valid) {
+    if (text_offset == 0xF2F2F2F2) { // Invalid event marker
         return event;
     }
 
     memcpy(event.data, span->data, EVENT_SIZE);
 
     usize text_size = EVENT_SIZE - text_offset;
-    span_t text_span = {
-        .data = event.data + text_offset,
-        .size = text_size,
-    };
-    event.messages_len = read_messages(&text_span, event.messages);
+    span_t text_span = { .data = event.data + text_offset, .size = text_size };
+    usize messages_len = read_messages(&text_span, event.messages);
 
     usize code_size = text_offset - 4;
-    span_t code_span = {
-        .data = event.data + 4,
-        .size = code_size,
-    };
-    event.instruction_count = read_instructions(&code_span, event.instructions);
+    span_t code_span = { .data = event.data + 4, .size = code_size };
+    usize instruction_count = read_instructions(&code_span, event.instructions);
 
-    event.valid = valid;
+    event.messages_len = messages_len;
+    event.instruction_count = instruction_count;
+    event.valid = true;
     return event;
 }
 
